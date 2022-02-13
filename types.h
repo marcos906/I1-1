7b7b60df01123df16bd8912b31b89792339d6582
heectoor022 <hector.labrador@estudiante.uam.es>
2022-02-13 10:59:06 -0800

    UPDATE

--- types.h
+++ types.h
@@ -10,7 +10,17 @@
 
 #ifndef TYPES_H
 #define TYPES_H
+/*
+  Estructuras definidas en type.h:
+    long Id: Para guardar la Id de los elementos del juego;
+    enum Bool: Asigna el valor 0 a FALSE y 1 a TRUE;
+    enum Status: Asigna el valor 0 a ERROR y 1 a OK;
+    enum Direction: Asigna el valor 0 a N, 1 a S, 2 a E y 3 a W;
+  Macros:
+    NO_ID = -1
+    WORD_SIZE = 1000
 
+*/
 #define WORD_SIZE 1000
 #define NO_ID -1
 
