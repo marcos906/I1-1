7764e9167395431dffb50d0529673d8299427a34
heectoor022 <hector.labrador@estudiante.uam.es>
2022-02-14 03:39:06 -0800

    update

--- object.h
+++ object.h
@@ -16,63 +16,51 @@ typedef struct _Object Object;
 #define MAX_SPACES 100
 #define FIRST_SPACE 1
 
-/**
-  * @brief It creates a new space
-  * @author Profesores PPROG
-  *
-  * space_create allocates memory for a new space
-  *  and initializes its members
-  * @param id the identification number for the new space
-  * @return a new space, initialized
+/*
+    @Descripcion: crea el objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Id
+    @return: EStructura objeto
   */
 Object* object_create(Id id);
 
-/**
-  * @brief It destroys a space
-  * @author Profesores PPROG
-  *
-  * space_destroy frees the previous memory allocation 
-  *  for a space
-  * @param space a pointer to the space that must be destroyed  
-  * @return OK, if everything goes well or ERROR if there was some mistake
+/*
+    @Descripcion: Destruye el objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Puntero a estructura Objeto
+    @return: OK o ERROR
   */
 STATUS object_destroy(Object* object);
 
-/**
-  * @brief It gets the id of a space
-  * @author Profesores PPROG
-  * 
-  * @param space a pointer to the space  
-  * @return the id of space
+/*
+    @Descripcion: Coge la Id del objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Puntero a estructura Objeto
+    @return: Id del objeto
   */
 Id object_get_id(Object* object);
 
-/**
-  * @brief It sets the name of a space
-  * @author Profesores PPROG
-  * 
-  * @param space a pointer to the space
-  * @param name a string with the name to store
-  * @return OK, if everything goes well or ERROR if there was some mistake 
+/*
+    @Descripcion: Nombra el objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Puntero a estructura Objeto y puntero a un char
+    @return: OK o ERROR
   */
 STATUS object_set_name(Object* object, char* name);
 
-/**
-  * @brief It gets the name of a space
-  * @author Profesores PPROG
-  * 
-  * @param space a pointer to the space
-  * @return  a string with the name of the space
+/*
+    @Descripcion: Coge el nombre de un objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Puntero a estructura Objeto
+    @return: Nombre del objeto
   */
-const char* object_get_name(Object* object;
+const char* object_get_name(Object* object);
 
-/**
-  * @brief It prints the space information
-  * @author Profesores PPROG
-  *
-  * This fucntion shows the id and name of the space, the spaces that surrounds it and wheter it has an object or not.
-  * @param space a pointer to the space
-  * @return OK, if everything goes well or ERROR if there was some mistake
+/*
+    @Descripcion: Muestra el objeto
+    @Autor: Héctor Labrador Gómez
+    @parametros: Puntero a estructura Objeto
+    @return: OK o ERROR
   */
 STATUS object_print(Object* object);
 
