63a0cae4e0457b897a98c42f3ef5097764f01afd
heectoor022 <hector.labrador@estudiante.uam.es>
2022-02-15 00:30:29 -0800

    update

--- game_loop.c
+++ game_loop.c
@@ -13,6 +13,7 @@
 #include "graphic_engine.h"
 #include "game.h"
 #include "command.h"
+#include "game_reader.h"
 
 int game_loop_init(Game *game, Graphic_engine **gengine, char *file_name);
 void game_loop_run(Game game, Graphic_engine *gengine);
